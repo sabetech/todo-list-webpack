ac0ba7106ffe186f2bba16010a3247a7299aca08
Albert <albert.mensahansah@gmail.com>
2022-03-31 07:56:10 +0000

    Add linters and refresh button

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 import _ from 'lodash';
 import './style.css';
-
+import RefreshIcon from './icons8-refresh-30.png';
 
 const todoListEl = document.querySelector('.todo-list-el');
-
-
+const imgRefreshEl = document.querySelector('.refresh-icon');
+imgRefreshEl.src = RefreshIcon;
 
 let todos = [
   {
-    description: "Hey",
+    description: 'Hey',
     completed: false,
-    index: 0
+    index: 0,
   },
   {
-    description: "spija koutj",
+    description: 'spija koutj',
     completed: false,
-    index: 1
-  }
-]
+    index: 1,
+  },
+];
 
 const showList = () => {
-  todos = _.orderBy(todos, ['index'],['asc']);
+  todos = _.orderBy(todos, ['index'], ['asc']);
   todos.map((todo) => {
     const input = document.createElement('input');
-    input.id = todo.index
-    input.type = "checkbox";
+    input.id = todo.index;
+    input.type = 'checkbox';
 
-    todoListEl.innerHTML += "<li><input type='checkbox'><label class='todo-desc'>"+ todo.description +"</label></li>";
-
+    todoListEl.innerHTML += `<li><input type='checkbox'><label class='todo-desc'>${todo.description}</label></li>`;
   });
-}
+};
 
 showList();
